325ac2e1b3414694cd009f267f039ecc46d3cb82
Gerrit-Jan de Bruin <gerritjan.debruin@gmail.com>
2021-08-11 08:57:25 +0200

    Fixes

--- run.sh
+++ run.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/with-contenv bashio
 
 FREQUENCY=$(bashio::config 'frequency')
-echo "frequency" $FREQUENCY
-
 DEVICE=$(bashio::config 'device')
-echo "device" $DEVICE
 
-# MQTT_PREFIX=$(bashio::config 'discovery_prefix_mqtt')
+MQTT_PREFIX=$(bashio::config 'discovery_prefix_mqtt')
 MQTT_HOST=$(bashio::services mqtt "host")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
-echo $MQTT_HOST $MQTT_USER $MQTT_PASSWORD
-# TOPIC=$(bashio::config 'topic_parent')
-# TYPE=$(bashio::config 'trigger_type')
-# SUBTYPE=$(bashio::config 'trigger_subtype')
-# MANUFACTURER=$(bashio::config 'manufacturer')
-# DEVICE_NAME=$(bashio::config 'device_name')
-# ID=$(bashio::config 'id')
-# MODEL=$(bashio::config 'model')
+TOPIC=$(bashio::config 'topic_parent')
+TYPE=$(bashio::config 'trigger_type')
+SUBTYPE=$(bashio::config 'trigger_subtype')
+MANUFACTURER=$(bashio::config 'manufacturer')
+DEVICE_NAME=$(bashio::config 'device_name')
+ID=$(bashio::config 'id')
+MODEL=$(bashio::config 'model')
 
-# MESSAGE="{\"automation_type\":\"trigger\",\"topic\":\"rtl_433/time\",\"type\":\"${TYPE}\",\"subtype\":\"${SUBTYPE}\",\"device\":{\"manufacturer\":\"${MANUFACTURER}\",\"name\":\"${DEVICE_NAME}\",\"identifiers\":\"${ID}\",\"model\":\"${MODEL}\"}}"
-mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -p $MQTT_PASSWORD -t "test" -m "test"
+TOPIC="${MQTT_PREFIX}/${ID}/config"
+MESSAGE="{\"automation_type\":\"trigger\",\"topic\":\"rtl_433/time\",\"type\":\"${TYPE}\",\"subtype\":\"${SUBTYPE}\",\"device\":{\"manufacturer\":\"${MANUFACTURER}\",\"name\":\"${DEVICE_NAME}\",\"identifiers\":\"${ID}\",\"model\":\"${MODEL}\"}}"
+mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASSWORD -t $TOPIC -m $MESSAGE
 
 # echo "TEST2"
 
