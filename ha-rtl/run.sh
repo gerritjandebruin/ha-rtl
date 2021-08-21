29c12e7aad17f1e306fd5ad42ae8cc99b962ec7e
Gerrit Jan de Bruin <gerritjan.debruin@gmail.com>
2021-08-21 11:02:56 +0200

    First experiment: rebuild on development

--- ha-rtl/run.sh
+++ ha-rtl/run.sh
@@ -1,22 +1,36 @@
 #!/usr/bin/with-contenv bashio
 
+# RTL_433 SETTINGS
 FREQUENCY=$(bashio::config 'frequency')
-DEVICE=$(bashio::config 'device')
+PROTOCOL=$(bashio::config 'device')
 
+# AUTO DISCOVERY
 MQTT_PREFIX=$(bashio::config 'discovery_prefix_mqtt')
+
+# MQTT
 MQTT_HOST=$(bashio::services mqtt "host")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
+
+# DEVICE SETTINGS
 TYPE=$(bashio::config 'trigger_type')
 SUBTYPE=$(bashio::config 'trigger_subtype')
 MANUFACTURER=$(bashio::config 'manufacturer')
 DEVICE_NAME=$(bashio::config 'device_name')
-ID=$(bashio::config 'id')
 MODEL=$(bashio::config 'model')
+IDENTIFIERS=$(bashio::config 'identifiers')
 
-TOPIC_DISCOVERY="${MQTT_PREFIX}/${ID}/config"
-TOPIC_STATE="${MQTT_PREFIX}/${ID}/state"
-MESSAGE_DISCOVERY="{\"automation_type\":\"trigger\",\"topic\":\"${TOPIC_STATE}/time\",\"type\":\"${TYPE}\",\"subtype\":\"${SUBTYPE}\",\"device\":{\"manufacturer\":\"${MANUFACTURER}\",\"name\":\"${DEVICE_NAME}\",\"identifiers\":\"${ID}\",\"model\":\"${MODEL}\"}}"
-mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASSWORD -t $TOPIC_DISCOVERY -m $MESSAGE_DISCOVERY
+MESSAGE_DISCOVERY="{\"automation_type\":\"trigger\",\"topic\":\"${MQTT_PREFIX}/rtl433/time\",\"type\":\"${TYPE}\",\"subtype\":\"${SUBTYPE}\",\"device\":{\"manufacturer\":\"${MANUFACTURER}\",\"name\":\"${DEVICE_NAME}\",\"identifiers\":\"${IDENTIFIERS}\",\"model\":\"${MODEL}\"}}"
+mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASSWORD -t $"${MQTT_PREFIX}/rtl433/config" -m $MESSAGE_DISCOVERY
 
-/usr/local/bin/rtl_433 -f $FREQUENCY -R $DEVICE -F "mqtt://${MQTT_HOST},user=${MQTT_USER},pass=${MQTT_PASSWORD},devices=${TOPIC_STATE}" -F kv -M level
+echo "TEST"
+echo $PROTOCOL
+
+if $PROTOCOL == ""
+then
+    echo "Nothing"
+else
+    echo "Error"
+fi
+
+# /usr/local/bin/rtl_433 -f $FREQUENCY -R $PROTOCOL -F "mqtt://${MQTT_HOST},user=${MQTT_USER},pass=${MQTT_PASSWORD},devices=${TOPIC_STATE}" -F kv -M level
