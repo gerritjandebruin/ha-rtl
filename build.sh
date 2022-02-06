a681afdf7d8aeee8ad7b42fa2e71a4b43a56a08b
Gerrit Jan de Bruin <gerritjan.debruin@gmail.com>
2022-02-06 16:49:37 +0100

    ⚰️ Remove unused file

--- build.sh
+++
@@ -1,9 +0,0 @@
-docker run \
-  --rm \
-  --privileged \
-  -v ~/.docker:/root/.docker \
-  homeassistant/amd64-builder \
-  --all \
-  -t ha-rtl \
-  -r https://github.com/gerritjandebruin/ha-rtl \
-  -b master
